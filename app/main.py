c4e419f23a3aa2ca93a6bb6e9279b6f365c8ba86
vkpande <vkpande@expediens.com>
2026-05-27 16:42:25 +0530

    added prometheus metrics

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 
 @app.get("/")
-async def home() -> dict:
-    """
-    Home endpoint.
-    """
+async def home():
     return {"message": "DevOps Practice Project"}
+
+
+Instrumentator().instrument(app).expose(app)
